ae64e4c5208a5cec4cb7ff8705c7e150599352bb
Roshinator <sevaliaroshan@gmail.com>
2022-01-07 19:21:15 -0800

    Cleaned up style to barebones

---
+++ generate-content.mjs
@@ -0,0 +1,38 @@
+//Credit to Ravbug for the format
+data = [
+    // {
+    //     title: "DevBlogs",
+    //     content:[
+    //         {
+    //             title:"RavEngine DevBlog",
+    //             img:"img/joystick.svg",
+    //             tag:"Recording my progress and thoughts while developing a custom game engine in C++",
+    //             ref:"blog/ravengine",
+    //         }
+    //     ]
+    // },
+    {
+        title:"Software Development",
+        content:[
+            {
+                title:"Game-Boy-Inator",
+                img:"images/game-boy.bmp",
+                tag:"My first emulation project.",
+                ref:"https://github.com/roshinator/game-boy-inator",
+            },
+            {
+                title:"Tankista",
+                img:"https://rrsoftware.github.io/images/appicon.png",
+                tag:"Mulitplayer tank battle game in development",
+                ref:"https://rrsoftware.github.io",
+            },
+            {
+                title:"CrossCorrupt",
+                img: "",
+                tag: "Cross platform file / folder corrupting tool, written in C# and Eto.forms.",
+                ref: "https://github.com/Ravbug/CrossCorrupt",
+            }
+        ]
+    },    
+]
+document.currentScript.remove()
